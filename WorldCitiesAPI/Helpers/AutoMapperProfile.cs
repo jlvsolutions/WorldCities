30b08668fc830adb5d87b0c30c6f6a17aae9703e
Jerry Virden <JerryVirden@outlook.com>
2022-10-25 20:56:17 -0500

    add hint comment to automapperprofile.

--- WorldCitiesAPI/Helpers/AutoMapperProfile.cs
+++ WorldCitiesAPI/Helpers/AutoMapperProfile.cs
@@ -26,6 +26,9 @@
                         if (prop == null) return false;
                         if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) return false;
 
+                        // ignore null role
+                        //if (x.DestinationMember.Name == "Role" && src.Role == null) return false;
+
                         return true;
                     }
                 ));
