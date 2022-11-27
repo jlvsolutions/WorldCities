3524b660a86fa08b370d6b15a21de190d581543c
Jerry Virden <JerryVirden@outlook.com>
2022-11-26 21:58:15 -0600

    Added sample wcmap.config.ts w/instructions.

--- WorldCities/src/app/_shared/wcmap/wcmap.sample.config.ts
+++ WorldCities/src/app/_shared/wcmap/wcmap.sample.config.ts
@@ -1,6 +1,6 @@
 /*
  * 1.  Rename this file to wcmap.config.ts.
- * 2.  Replace apiKey value with your Google Embedded Map ASI key.
+ * 2.  Replace apiKey value with your Google Embedded Map API key.
  *
  */
 
