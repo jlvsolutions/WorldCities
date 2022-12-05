da47d524fe760e01c72a3548e9d465f8c54e33d8
Jerry Virden <JerryVirden@outlook.com>
2022-12-04 21:29:54 -0600

    Added functionality to support reactive objects.

--- WorldCities/src/app/_models/ishow-message.ts
+++ WorldCities/src/app/_models/ishow-message.ts
@@ -1,3 +1,5 @@
+import { Observable, Subject, BehaviorSubject, tap, map } from 'rxjs';
+
 /** Interface for components that show an informational message */
 export interface IShowMessage {
   /** Shows given informational message */
@@ -8,4 +10,12 @@ export interface IShowMessage {
   set errMessage(value: string);
   /** Clears any message from showing */
   clear(): void;
+  connect(message: Observable<Message>): void;
+  disconnect(): void;
+}
+
+export type MessageType = 'error' | 'info' | 'spinner' | 'none';
+export class Message {
+  type: MessageType = 'none';
+  message: string = '';
 }
--- WorldCities/src/app/_shared/show-message/show-message.component.ts
+++ WorldCities/src/app/_shared/show-message/show-message.component.ts
@@ -1,29 +1,20 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { IShowMessage } from '@app/_models';
+import { IShowMessage, Message, MessageType } from '@app/_models';
+import { Observable, takeUntil, Subject, BehaviorSubject, tap, map } from 'rxjs';
 
 /** Shows messages to users as informative or as an error. */
 @Component({
   selector: 'show-message',
   templateUrl: './show-message.component.html',
-  styles: [`
-    .showMessageLabel {
-      font-size: large;
-      color: steelblue;
-      display: flex;
-      align-items: center;
-      justify-content: left;
-    }
-    .spinner: {
-      display: inline-block;
-      line-height: 30px;
-    }
-`]
+  styleUrls: ['./show-message.component.scss']
 })
-export class ShowMessageComponent implements IShowMessage {
+export class ShowMessageComponent implements IShowMessage, OnDestroy {
   
   _msg: string = '';
   _errMsg: string = '';
   _spinner: boolean = false;
+  _message?: Message;
+  private destroySubject = new Subject();
 
   constructor() { }
 
@@ -45,10 +36,39 @@ export class ShowMessageComponent implements IShowMessage {
     this._spinner = false;
   }
 
-
   clear() {
     this._msg = '';
     this._errMsg = '';
     this._spinner = false;
+  }
+
+  connect(message: Observable<Message>): void {
+    message
+      .pipe(takeUntil(this.destroySubject))
+      .subscribe(m => {
+      switch (m.type) {
+        case 'none':
+          this.clear();
+          break;
+        case 'info':
+          this.message = m.message;
+          break;
+        case 'error':
+          this.errMessage = m.message;
+          break;
+        case 'spinner':
+          this.spinner = m.message;
+          break;
+      }
+    });
+  }
+
+  disconnect(): void {
+    this.destroySubject.next(true);
+    this.destroySubject.complete();
+  }
+
+  ngOnDestroy(): void {
+    this.disconnect();
   }
 }
